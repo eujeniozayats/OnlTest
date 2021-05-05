c795cdb470377b3159ac76f9c72cd3b2fc07273a
eujeniozayats <yauheni.zayats@itechart-group.com>
2021-05-05 18:53:50 +0300

    commit

--- src/test/java/Framework/Checkbox.java
+++ src/test/java/Framework/Checkbox.java
@@ -7,17 +7,19 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import testcases.TestBase;
+import utils.ConfigFileReader;
 
 public interface Checkbox {
     final WebDriver driver = TestBase.driver;
     final JavascriptExecutor js = (JavascriptExecutor) TestBase.driver;
+    final ConfigFileReader reader = new ConfigFileReader();
 
 
     public static void check(WebElement element) {
-        (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(element));
+        (new WebDriverWait(driver, reader.getImplicitlyWait())).until(ExpectedConditions.elementToBeClickable(element));
         js.executeScript("arguments[0].scrollIntoView();", element);
         element.click();
-        (new WebDriverWait(driver, 10)).until((ExpectedCondition<Boolean>) wd ->
+        (new WebDriverWait(driver, reader.getImplicitlyWait())).until((ExpectedCondition<Boolean>) wd ->
                 ((JavascriptExecutor) TestBase.driver).executeScript("return document.readyState").equals("complete"));
     }
 }
